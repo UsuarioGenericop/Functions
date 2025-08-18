e019e4e6e493c8a9c1ffabd781ebcb99200be361
Andres <andydquintero@gmail.com>
2025-08-17 21:08:46 -0500

    TypesOfFunctions

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function example(a, b, c) {
+  return a + b + c;
+}
+console.log(example(1, 2, 3));
+
+example = function (a, b, c) {
+  return a + b - c;
+}; //Expression Function
+console.log(example(1, 2, 3));
+
+let suma = function (a, b) {
+  return a + b;
+};
+console.log(suma(1, 2));
+suma = (a, b) => {
+  return a - b;
+}; //Arrow Function
+console.log(suma(1, 2));
